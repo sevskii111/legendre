d5570f98ff6b8fddc3eaaa1dd9fc8ac7f72b5610
sevskii <sevskii111@gmail.com>
2021-10-18 11:59:38 +0300

    minor fixes

--- eval.py
+++ eval.py
@@ -7,8 +7,9 @@ def evaluate(expression, vars=None):
         p = Parser(expression, vars)
         value = p.getValue()
     except Exception as ex:
-        msg = ex.message
-        raise Exception(msg)
+        raise Exception()
+        # msg = ex.message
+        # raise Exception(msg)
 
     # Return an integer type if the answer is an integer
     if int(value) == value:
